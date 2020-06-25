b1dad5d1ce5c11b8c9b4bd51d4226f1600c79317
Frank Vasek <frnk05vas@zoho.com>
2020-06-25 13:55:18 -0700

    finished app

--- number-facts/number-facts-app.js
+++ number-facts/number-facts-app.js
@@ -1,24 +1,4 @@
 // number facts app
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**
  * Part 1: Number Facts
 Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API. Details.
@@ -29,3 +9,60 @@ Use the API to get 4 facts on your favorite number. Once you have them all, put 
 
 (Note: You’ll need to make multiple requests for this.)
  */
+
+const doLuckyNumberFacts = (luckyNumber, results=[]) => {
+    if(results.length == 4) {
+        const ul = document.getElementById("lucky-number-ul");
+        results.map(e => {
+            let li = document.createElement("LI");
+            li.innerText = e;
+            ul.append(li);
+        });
+    } else {
+        axios({
+            method: "GET", 
+            url: `http://numbersapi.com/${luckyNumber}/trivia?json`
+        }).then(response => {
+            results.push(response.data.text);
+            doLuckyNumberFacts(luckyNumber, results);
+        }).catch(error => {
+            reject(error);
+        });
+    }
+    
+}
+
+const doListNumberFacts = numberList => {
+    axios({
+        method: "GET", 
+        url: `http://numbersapi.com/${numberList}/trivia?json`
+    }).then(response => {
+        const ul = document.getElementById("list-number-ul");
+        Object.values(response.data).map(e => {
+            let li = document.createElement("LI");
+            li.innerText = e;
+            ul.append(li);
+        });
+    }).catch(error => {
+        reject(error);
+    });
+    
+}
+
+document.getElementById("lucky-number-form").addEventListener("click", async e => {
+    e.preventDefault();
+    if(e.target.id == "btn-submit") {
+        const luckyNumber = document.getElementById("lucky-number").value;
+        doLuckyNumberFacts(luckyNumber);
+    }
+});
+
+document.getElementById("list-number-form").addEventListener("click", async e => {
+    e.preventDefault();
+    if(e.target.id == "btn-submit") {
+        let luckyNumber = document.getElementById("list-number").value;
+        luckyNumber = luckyNumber.replace(/\s/g, "");
+        console.log(luckyNumber)
+        doListNumberFacts(luckyNumber);
+    }
+});
