b990bc5eab7cb0cab40a4379948437b41045d5a5
Frank Vasek <frnk05vas@zoho.com>
2020-06-25 20:18:21 -0700

    finished with app

--- deck-of-cards/app.js
+++ deck-of-cards/app.js
@@ -9,28 +9,46 @@ Once you have both cards, console.log the values and suits of both cards.
 
 Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
  */
-const canvas = document.querySelector(".display-canvas");
-const BASE_URL = "https://deckofcardsapi.com/api/deck/";
-// const SHUFFLE = "https://deckofcardsapi.com/api/deck/<<deck_id>>/shuffle/";
-// const DRAW =    "https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2";
 
+const BASE_URL = "https://deckofcardsapi.com/api/deck/";
+
+let deckId = 0;
+let rot = 0;
+let remaining = 52;
 
 const getCard = () => {
-    axios({
-        method: "get",
-        url: `${BASE_URL}new/draw`
-    }).then(response =>{
-        console.log(response.data.deck_id)
-        
-        let imgsrc = response.data.cards[0].image;
-        console.log(imgsrc, typeof imgsrc)
-        console.log(response.data.remaining)
-        let ctx = canvas.getContext("2d");
-        let img = document.createElement("IMG");
-        img.src = "http://deckofcardsapi.com/static/img/5C.png"
-        ctx.drawImage(img, 200, 200);
-    }).catch(error => {
-        console.error(error);
-    });
+    if(remaining) {
+        if(!deckId) {
+            url = `${BASE_URL}new/draw`
+        } else {
+            url = `${BASE_URL}${deckId}/draw/?count=1`
+        }
+        axios({
+            method: "get",
+            url: url
+    
+        }).then(response =>{
+            deckId = response.data.deck_id;
+            remaining = response.data.remaining;
+            if(this.rot < 180) this.rot += 30;
+            else this.rot = 30;
+            const display = document.querySelector(".display")
+            let img = new Image();
+            img.crossOrigin = "anonymous";
+            img.style.transform = `rotate(${this.rot}deg)`;
+            img.src = response.data.cards[0].image;
+            display.append(img);
+        }).catch(error => {
+            console.error(error);
+        });
+    }    
 }
-getCard();
+document.querySelector("button").addEventListener("click", () => {
+    
+        getCard();
+});
+
+
+
+
+
