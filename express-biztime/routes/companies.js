5f458a1972e8aa24f5ee320a3f9895267cb3f1d3
Frank Vasek <frnk05vas@zoho.com>
2020-07-13 09:22:09 -0700

    adding updates

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -1,1 +1,75 @@
+const express = require("express");
+const router = new express.Router();
 
+const db = require("../db");
+
+// biztime=# select * from companies
+// biztime-# ;
+//  code  |      name      |  description
+// -------+----------------+---------------
+//  apple | Apple Computer | Maker of OSX.
+//  ibm   | IBM            | Big blue.
+
+// GET /companies
+//   Returns list of companies, like {companies: [{code, name}, ...]}
+router.get("/companies", async function (req, res, next) {
+  try {
+    console.log("--------------------------------------")
+    // const results = await db.query(`SELECT * FROM companies`);
+    // return res.json(results.rows);
+    return res.json({companies: [{code, name}]})
+  } catch(err) {
+    return next(err);
+  }
+});
+
+// GET /companies/[code]
+//   Return obj of company: {company: {code, name, description}}
+router.get("/companies/:code", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+//   If the company given cannot be found, this should return a 404 status response.
+
+// POST /companies
+//   Adds a company.
+//   Needs to be given JSON like: {code, name, description}
+//   Returns obj of new company: {company: {code, name, description}}
+router.post("/companies", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// PUT /companies/[code]
+//   Edit existing company.
+//   Should return 404 if company cannot be found.
+//   Needs to be given JSON like: {name, description}
+//   Returns update company object: {company: {code, name, description}}
+router.put("/companies/:code", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// DELETE /companies/[code]
+//   Deletes company.
+//   Should return 404 if company cannot be found.
+//   Returns {status: "deleted"}
+router.delete("/companies/:code", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+module.exports = router;
--- express-biztime/routes/invoices.js
+++ express-biztime/routes/invoices.js
@@ -1,1 +1,80 @@
+const express = require("express");
+const router = new express.Router();
 
+const db = require("../db");
+
+// biztime=# select * from invoices;
+//  id | comp_code | amt | paid |  add_date  | paid_date
+// ----+-----------+-----+------+------------+------------
+//   1 | apple     | 100 | f    | 2020-07-13 |
+//   2 | apple     | 200 | f    | 2020-07-13 |
+//   3 | apple     | 300 | t    | 2020-07-13 | 2018-01-01
+//   4 | ibm       | 400 | f    | 2020-07-13 |
+// GET /invoices
+//   Return info on invoices: like {invoices: [{id, comp_code}, ...]}
+router.get("/invoices", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// GET /invoices/[id]
+//   Returns obj on given invoice.
+//   If invoice cannot be found, returns 404.
+//   Returns {invoice: {id, amt, paid, add_date, paid_date, company: {code, name, description}}}
+router.get("/invoices/:id", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// POST /invoices
+//   Adds an invoice.
+//   Needs to be passed in JSON body of: {comp_code, amt}
+//   Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+router.post("/invoices", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// PUT /invoices/[id]
+//   Updates an invoice.
+//   If invoice cannot be found, returns a 404.
+//   Needs to be passed in a JSON body of {amt}
+//   Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+router.put("/invoices/:id", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+// DELETE /invoices/[id]
+//   Deletes an invoice.
+//   If invoice cannot be found, returns a 404.
+//   Returns: {status: "deleted"}
+router.delete("/invoices/:id", async (req, res, next) => {
+  // try {
+
+  // } catch(err) {
+  //   return next(err);
+  // }
+});
+
+
+// Also, one route from the previous part should be updated:
+
+// GET /companies/[code]
+// Return obj of company: {company: {code, name, description, invoices: [id, ...]}}
+
+// If the company given cannot be found, this should return a 404 status response.
+
+module.exports = router;
