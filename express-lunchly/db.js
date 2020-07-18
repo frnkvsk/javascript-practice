a4dff45c8599993b565d100530420c8ee4a8cb9e
Frank Vasek <frnk05vas@zoho.com>
2020-07-17 18:57:31 -0700

    updated db.js

--- express-lunchly/db.js
+++ express-lunchly/db.js
@@ -3,17 +3,10 @@
 const pg = require("pg");
 
 // database username
-<<<<<<< HEAD
-const databaseUserName = "postgres";
-
-// database user password
-const databaseUserPassword = "springboard";
-=======
 const databaseUserName = "";
 
 // database user password
 const databaseUserPassword = "";
->>>>>>> 5ba64247c4d61d0bef4cbe43644be9f18f721c81
 
 // port
 const port = "5432";
@@ -24,13 +17,6 @@ if (process.env.NODE_ENV === "test") {
 } else {
   DB_URI += "lunchly";
 }
-<<<<<<< HEAD
-=======
-
-let db = new pg.Client({
-  connectionString: DB_URI
-});
->>>>>>> 5ba64247c4d61d0bef4cbe43644be9f18f721c81
 
 let db = new pg.Client({
   connectionString: DB_URI
