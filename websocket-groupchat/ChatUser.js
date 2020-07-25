918bf33d26a142d3e339e8f518423487288d41a6
Frank Vasek <frnk05vas@zoho.com>
2020-07-25 13:29:43 -0700

    finished adding joke feature

---
+++ websocket-groupchat/ChatUser.js
@@ -0,0 +1,104 @@
+/** Functionality related to chatting. */
+
+// Room is an abstraction of a chat channel
+const Room = require('./Room');
+const { text } = require('express');
+const axios = require("axios");
+/** ChatUser is a individual connection from client -> server to chat. */
+
+class ChatUser {
+  /** make chat: store connection-device, rooom */
+
+  constructor(send, roomName) {
+    this._send = send; // "send" function for this user
+    this.room = Room.get(roomName); // room user will be in
+    this.name = null; // becomes the username of the visitor
+
+    console.log(`created chat in ${this.room.name}`);
+  }
+
+  /** send msgs to this client using underlying connection-send-function */
+
+  send(data) {
+    try {
+      this._send(data);
+    } catch(err) {
+      console.error(err)
+      // If trying to send to a user fails, ignore it
+    }
+  }
+
+  /** handle joining: add to room members, announce join */
+
+  handleJoin(name) {
+    this.name = name;
+    this.room.join(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} joined "${this.room.name}".`
+    });
+  }
+
+  /** handle a chat: broadcast to room. */
+
+  handleChat(text) {
+    console.log("handleChat")
+    this.room.broadcast({
+      name: this.name,
+      type: 'chat',
+      text: text
+    });
+  }
+
+  /** handle a joke: DON't broadcast to room. */
+
+  async handleJoke() {
+    console.log("0handlejoke")
+    const config = {
+      method: "get", 
+      url: "https://icanhazdadjoke.com/", 
+      headers: {"Accept": "application/json"}
+    };
+    let res = await axios(config);
+    console.log("/n/n ",res.data)
+    this.room.singlecast({
+      name: this.name,
+      type: 'chat',
+      text: res.data.joke
+    });
+  
+    // console.log("2handleJoke joke",joke)
+    // this.room.broadcast({
+    //   name: this.name,
+    //   type: 'chat',
+    //   text: joke
+    // });
+  }
+
+  /** Handle messages from client:
+   *
+   * - {type: "join", name: username} : join
+   * - {type: "chat", text: msg }     : chat
+   */
+
+  handleMessage(jsonData) {
+    console.log("handleMessage")
+    let msg = JSON.parse(jsonData);
+    if (msg.text === '/joke') this.handleJoke();
+    else if (msg.type === 'join') this.handleJoin(msg.name);
+    else if (msg.type === 'chat') this.handleChat(msg.text);
+    else throw new Error(`bad message: ${msg.type}`);
+  }
+
+  /** Connection was closed: leave room, announce exit to others */
+
+  handleClose() {
+    this.room.leave(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} left ${this.room.name}.`
+    });
+  }
+}
+
+module.exports = ChatUser;
