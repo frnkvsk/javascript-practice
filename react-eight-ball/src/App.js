95ba2b76206282dc23da1b40437cb6d2c4adb8f4
frnkvsk <frnk05vas@zoho.com>
2020-08-11 09:03:51 -0700

    fixed setState functionality

--- react-eight-ball/src/App.js
+++ react-eight-ball/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import EightBall from './EightBall';
-import ColorBox from './ColorBox';
+import ColorBoxes from './ColorBoxes';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
       <EightBall />
-      <ColorBox />
+      <ColorBoxes />
     </div>
   );
 }
--- react-eight-ball/src/ColorBoxes.js
+++ react-eight-ball/src/ColorBoxes.js
@@ -1,23 +1,28 @@
 import React, {useState} from 'react';
 import './ColorBox.css';
 
-const ColorBox = () => {
+const ColorBox = (props) => <li style={{ background: props.color }} />;
+
+const ColorBoxes = () => {
   const colors = ["red", "green", "blue", "orange", "cyan", "purple", "yellow", "chartreuse", "aqua", "blueviolet", "cornflowerblue", "greenyellow", "gold", "violet", "skyblue", "salmon"];
-  let [boxes, setBoxes] = useState(colors.map((e,i) => <li key={e+i} className={e}></li>) );
+
+  let [boxes, setBoxes] = useState(colors.slice());
+
   const random = () => Math.floor(Math.random() * colors.length);
-  const colorNextBox = () => {    
-    let rand = random();
-    boxes[rand] = <li key={boxes[rand].key} className={colors[random()]}></li>;
-    setBoxes(boxes.map(e => e));
+
+  const colorNextBox = () => {   
+    boxes[random()] = colors[random()];
+    setBoxes(boxes.slice());
   }
+  
   return (
     <div className="ColorBox">
       <ul className="ColorBox-ul">
-        {boxes}
+        {boxes.map(c => <ColorBox color={c} />)}
       </ul>
       <button className="ColorBox-button" onClick={colorNextBox}>Change</button>
     </div>
   )
 }
 
-export default ColorBox
+export default ColorBoxes
